0ce1d380e89ec17ae6af0ecd83717dfbcf5770b0
João Paulo Sartori <tec.joaosartori@gmail.com>
2025-02-23 14:53:01 -0300

    delet font

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function toggleMode() {
+  // Seleciona o elemento <html>
   const html = document.documentElement
+  // Alterna a classe "light"
   html.classList.toggle("light")
 
+  // Seleciona a imagem do perfil
   const img = document.querySelector("#profile img")
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    img.setAttribute("src", "./assets/avatar.png")
+  if (img) {
+    // Verifica se a imagem existe
+    // Altera o atributo src da imagem com base na presença da classe "light"
+    if (html.classList.contains("light")) {
+      img.setAttribute("src", "./assets/avatar-light.png")
+    } else {
+      img.setAttribute("src", "./assets/avatar.png")
+    }
   }
 }
